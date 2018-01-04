80b3451cf9fa65fc046c87b0b23f1088d821a22e
pjadda <pjadda@pjaddas-MBP.fios-router.home>
2018-01-04 12:56:00 -0500

    New/Edit not working as expected. Show and index files working

--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'projects/new', to:  'projects#new', as: :projects_index_path
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
